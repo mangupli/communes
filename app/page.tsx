4118010844d6cc4f2c3eef80f4641b4dea3674df
mangupli <elizaveta.mangupli@gmail.com>
2024-01-31 11:51:29 +1100

    update root page – in progress

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Find your community to create projects together. Be a part of commune
+      Find your community to create projects together. Be a part of commune.
+      <i>The page is in progress...</i>
     </main>
   );
 }
